d49bd071831e822f4cbaf8b1ae0fdd171cbd9e38
daniel16-bit <camargocamargodaniel0@gmail.com>
2025-09-22 11:40:22 -0500

    Update Dashboard.php

--- administradorphp/Dashboard.php
+++ administradorphp/Dashboard.php
@@ -238,7 +238,8 @@ if(!isset($_SESSION['Prime_Nombre'])){
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js"></script>
-
+    <script src="js/scripts.js"></script>
+    <script src="js/datatables-simple-demo.js"></script>
 <script>
     // Inicializar DataTables
     const dataTable = new simpleDatatables.DataTable("#datatablesSimple");
